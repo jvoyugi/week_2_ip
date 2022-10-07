46d5a3a33a55092be46e0bb599ddc2dc1e76a262
Javan Oyugi <jooyugi@safaricom.co.ke>
2022-10-07 18:53:48 +0300

    extract inputs

---
+++ assets/js/scripts.js
@@ -0,0 +1,15 @@
+const MALE_AKAN_NAMES = [
+  "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"
+]
+
+const FEMALE_AKAN_NAMES = [
+  "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"
+]
+
+function getName() {
+  let year = document.getElementById('year');
+  let month = document.getElementById('month');
+  let day = document.getElementById('day');
+  let gender = document.querySelector('input[name="gender"]:checked').value;
+  
+}
